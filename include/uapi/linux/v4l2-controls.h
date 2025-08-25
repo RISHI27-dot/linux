e7ce2a40f1ed9f99b02204d03dbf53e569e276ea
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-08-25 12:50:47 +0300

    media: uapi: Add metadata layout for ov2740 embedded data
    Add a metadata layout for ov2740 camera sensor embedded data and document
    it.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Julien Massot <julien.massot@collabora.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -1254,6 +1254,7 @@ enum v4l2_jpeg_chroma_subsampling {
 
 #define V4L2_CID_METADATA_LAYOUT		(V4L2_CID_IMAGE_SOURCE_CLASS_BASE + 12)
 #define V4L2_METADATA_LAYOUT_CCS		1U
+#define V4L2_METADATA_LAYOUT_OV2740		2U
 
 
 /* Image processing controls */
